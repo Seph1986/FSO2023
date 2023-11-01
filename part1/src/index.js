a7669e421bf500b58c82d5b06155627fcdc9abcb
Jose <jose.weber97@hotmail.com>
2023-11-01 16:26:37 -0300

    without help

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}> {props.text} </button>
+const Title = ({title}) => <h1>{title}</h1>
+
+const Button = ({handler,text}) => (
+  <button onClick={handler}>{text}</button>
 )
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  const [good, goodAction] = useState(0)
+  const [neutral, neutralAction] = useState(0)
+  const [bad, badAction] = useState(0)
 
-  const setNewValue = (newValue) => {
-    setValue(newValue)
-  }
 
   return (
     <div>
-      {value}
-      <Button handleClick={()=> setNewValue(1000)} text='thousand' />
-      <Button handleClick={() => setNewValue(0)} text='reset' />
-      <Button handleClick={() => setNewValue(value+1)} text='increment' />
+      <Title title='give feedback' />
+      <Button handler={()=>goodAction(good + 1)} text='good' />
+      <Button handler={()=>neutralAction(neutral + 1)} text='neutral' />
+      <Button handler={()=>badAction(bad + 1)} text='bad' />
+      <Title title='statistics' />
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
