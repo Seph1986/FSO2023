ba86c5c55b79307f77c5127e78723591697f58df
Jose <jose.weber97@hotmail.com>
2023-11-01 18:52:08 -0300

    before statistics

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,31 +3,60 @@ import ReactDOM from 'react-dom'
 
 const Title = ({title}) => <h1>{title}</h1>
 
-const Button = ({handler,text}) => (
-  <button onClick={handler}>{text}</button>
+const Button = ({handler,text}) =>(
+  <button onClick={handler} >{text}</button>
 )
 
-const App = () => {
-  const [good, goodAction] = useState(0)
-  const [neutral, neutralAction] = useState(0)
-  const [bad, badAction] = useState(0)
+const Display = ({text,number}) => <div>{text} {number}</div>
 
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0) 
+  const [avg, setProm] = useState(0)
+  const [pos, setPositive] = useState(0)
+
+
+  const handleGood = () =>{
+    setGood(good + 1)
+    setAll(all + 1)
+    setProm(avg + 1)
+    setPositive(( (good + 1) * 100 ) / (all + 1))
+  }
+
+  const handleNeutral = () =>{
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setProm((avg + 0))
+    setPositive(( good * 100 ) / (all + 1))
+  }
+  
+  const handleBad = () =>{
+    setBad(bad + 1)
+    setAll(all + 1)
+    setProm(avg - 1)
+    setPositive(( good * 100 ) / (all + 1))
+  }
 
   return (
     <div>
       <Title title='give feedback' />
-      <Button handler={()=>goodAction(good + 1)} text='good' />
-      <Button handler={()=>neutralAction(neutral + 1)} text='neutral' />
-      <Button handler={()=>badAction(bad + 1)} text='bad' />
+      <Button handler={handleGood} text='good' />
+      <Button handler={handleNeutral} text='neutral' />
+      <Button handler={handleBad} text='bad' />
       <Title title='statistics' />
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Display text='good' number={good} />
+      <Display text='neutral' number={neutral} />
+      <Display text='bad' number={bad} />
+      <Display text='all' number={all} />
+      <Display text='average' number={avg} />
+      <Display text='positive' number={pos} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />, 
+ReactDOM.render(<App />, 
   document.getElementById('root')
 )
