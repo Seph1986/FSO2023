169389aaa1eba45ce0402ffc7f352cab5b1157a8
Jose <jose.weber97@hotmail.com>
2023-11-01 19:43:59 -0300

    not sure looks like first part

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,7 +7,37 @@ const Button = ({handler,text}) =>(
   <button onClick={handler} >{text}</button>
 )
 
-const Display = ({text,number}) => <div>{text} {number}</div>
+const StatisticLine = ({text,value}) => {
+  return(
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+  )
+}
+
+const Statistics = (props) => {
+  
+  if (props.all === 0) return <h3>No feedback given</h3>
+
+  return(
+    <div>
+      <h1>statisctics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={props.good}/>
+          <StatisticLine text='neutral' value={props.neutral} /> 
+          <StatisticLine text='bad' value={props.bad} />
+          <StatisticLine text='all' value={props.all} /> 
+          <StatisticLine text='average' value={props.avg} /> 
+          <StatisticLine text='positive' value={props.pos} /> 
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -15,28 +45,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0) 
-  const [avg, setProm] = useState(0)
+  const [avg, setAvg] = useState(0)
   const [pos, setPositive] = useState(0)
 
 
   const handleGood = () =>{
     setGood(good + 1)
     setAll(all + 1)
-    setProm(avg + 1)
+    setAvg(((good +1)-bad)/(all + 1))
     setPositive(( (good + 1) * 100 ) / (all + 1))
   }
 
   const handleNeutral = () =>{
     setNeutral(neutral + 1)
     setAll(all + 1)
-    setProm((avg + 0))
+    setAvg((good-bad)/(all + 1))
     setPositive(( good * 100 ) / (all + 1))
   }
   
   const handleBad = () =>{
     setBad(bad + 1)
     setAll(all + 1)
-    setProm(avg - 1)
+    setAvg((good-(bad + 1))/(all + 1))
     setPositive(( good * 100 ) / (all + 1))
   }
 
@@ -46,13 +76,7 @@ const App = () => {
       <Button handler={handleGood} text='good' />
       <Button handler={handleNeutral} text='neutral' />
       <Button handler={handleBad} text='bad' />
-      <Title title='statistics' />
-      <Display text='good' number={good} />
-      <Display text='neutral' number={neutral} />
-      <Display text='bad' number={bad} />
-      <Display text='all' number={all} />
-      <Display text='average' number={avg} />
-      <Display text='positive' number={pos} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} pos={pos} />
     </div>
   )
 }
