626b932cb1febb5de0ec2b6dfc518bfade8562e9
Jose <jose.weber97@hotmail.com>
2023-11-02 17:59:26 -0300

    last exercise

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,10 +3,49 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(
+    new Array(props.anecdotes.length).fill(0)
+  )
+  console.log(props)
+
+  const randomAnecdote = () =>{
+    let number = Math.floor(Math.random() * (props.anecdotes.length))
+    
+    return setSelected(number)
+  }
+
+  const addPoints = (number) =>{
+    return () => {
+      let newArray = [...points]
+      newArray[number] += 1
+
+      return setPoints(newArray)
+    }
+  }
+
+  const mostVotedAnecdote = () =>{
+    let max = Math.max(...points)
+    return points.indexOf(max)
+  }
+
+  const mostVotedIndex = mostVotedAnecdote();
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <div>
+        <p>{props.anecdotes[selected]}</p>
+        <p>has {points[selected]} votes</p>
+      </div>
+      <div>
+        <button onClick={randomAnecdote}>Next anecdote</button>
+        <button onClick={addPoints(selected)}>Vote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <div>
+        <p>{props.anecdotes[mostVotedIndex]}</p>
+        <p>{points[mostVotedIndex]}</p>
+      </div>
     </div>
   )
 }
